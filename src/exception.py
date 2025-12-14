25da81e391b9384f57d88b8885c99b989296f167
sanjay-jmp <sanjayjithesh@gmail.com>
2025-12-15 01:08:42 +0530

    logging and exception created

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-
+from logger import logging
 def error_message_detail(error, error_detail:sys):
     _,_, exc_tb = error_detail.exc_info() # exc_tb info holds the details such as in which fille the error the file was raised, which line number, etc
 
@@ -12,8 +12,9 @@ def error_message_detail(error, error_detail:sys):
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
+    
