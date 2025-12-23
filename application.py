48bc8596979946a572368d185d3f6b2ba5d17820
sanjay-jmp <sanjayjithesh@gmail.com>
2025-12-24 05:03:34 +0530

    Added safeguards in application

--- application.py
+++ application.py
@@ -16,25 +16,29 @@ def index():
 
 @app.route('/predictdata', methods = ['GET', 'POST'])
 def predict_datapoint():
-    if request.method == 'GET':
-        return render_template('home.html')
-    else:
-        data = CustomData(
-            gender = request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score')),
-        )
+    try:
+        if request.method == 'GET':
+            return render_template('home.html')
+        else:
+            data = CustomData(
+                gender = request.form.get('gender'),
+                race_ethnicity=request.form.get('ethnicity'),
+                parental_level_of_education=request.form.get('parental_level_of_education'),
+                lunch=request.form.get('lunch'),
+                test_preparation_course=request.form.get('test_preparation_course'),
+                reading_score=float(request.form.get('writing_score')),
+                writing_score=float(request.form.get('reading_score')),
+            )
 
-        pred_df = data.get_data_as_frame()
-        print(pred_df)
+            pred_df = data.get_data_as_frame()
+            print(pred_df)
 
-        predict_pipeline = PredictPipeline()
-        results = predict_pipeline.predict(pred_df)
-        return render_template('home.html', results=results[0])
+            predict_pipeline = PredictPipeline()
+            results = predict_pipeline.predict(pred_df)
+            return render_template('home.html', results=results[0])
+    except Exception as e:
+        import traceback
+        return f"<pre>{traceback.format_exc()}</pre>"
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
